bee51cda8a3caa287153605003ec65ccd462a348
NgTPhu <ngth.phu2608@gmail.com>
2023-10-18 04:34:47 +0700

    Di ngu thoi

--- QuanLyThuVienNhom05/BangCap.Designer.cs
+++ QuanLyThuVienNhom05/BangCap.Designer.cs
@@ -28,19 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvShow = new System.Windows.Forms.DataGridView();
+            this.MaBangCap = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenBangCap = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtBangCap = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bằng Cấp";
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(135, 62);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(154, 22);
+            this.txtTimKiem.TabIndex = 2;
+            this.txtTimKiem.TextChanged += new System.EventHandler(this.txtTimKiem_TextChanged);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(27, 196);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 3;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(145, 196);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 4;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(255, 196);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 5;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tìm kiếm";
+            // 
+            // dgvShow
+            // 
+            this.dgvShow.AllowUserToAddRows = false;
+            this.dgvShow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShow.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaBangCap,
+            this.TenBangCap});
+            this.dgvShow.Location = new System.Drawing.Point(411, 28);
+            this.dgvShow.Name = "dgvShow";
+            this.dgvShow.RowHeadersWidth = 51;
+            this.dgvShow.RowTemplate.Height = 24;
+            this.dgvShow.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvShow.Size = new System.Drawing.Size(389, 411);
+            this.dgvShow.TabIndex = 7;
+            this.dgvShow.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvShow_CellClick);
+            // 
+            // MaBangCap
+            // 
+            this.MaBangCap.HeaderText = "Mã Bằng Cấp";
+            this.MaBangCap.MinimumWidth = 6;
+            this.MaBangCap.Name = "MaBangCap";
+            this.MaBangCap.Width = 130;
+            // 
+            // TenBangCap
+            // 
+            this.TenBangCap.HeaderText = "Tên Bằng Cấp";
+            this.TenBangCap.MinimumWidth = 6;
+            this.TenBangCap.Name = "TenBangCap";
+            this.TenBangCap.Width = 200;
+            // 
+            // txtBangCap
+            // 
+            this.txtBangCap.Location = new System.Drawing.Point(135, 127);
+            this.txtBangCap.Name = "txtBangCap";
+            this.txtBangCap.Size = new System.Drawing.Size(154, 22);
+            this.txtBangCap.TabIndex = 1;
+            this.txtBangCap.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBangCap_KeyPress);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // BangCap
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(898, 451);
+            this.Controls.Add(this.dgvShow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.txtBangCap);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "BangCap";
             this.Text = "Bằng Cấp";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvShow;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaBangCap;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenBangCap;
+        private System.Windows.Forms.TextBox txtBangCap;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
