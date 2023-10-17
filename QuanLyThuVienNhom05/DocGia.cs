7c303154e59d0f3922e1814539b1df69a08e553d
Khang <mkhoangvip@gmail.com>
2023-10-17 23:51:35 +0700

    sua docgia

--- QuanLyThuVienNhom05/DocGia.cs
+++ QuanLyThuVienNhom05/DocGia.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyThuVienNhom05.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,8 @@ namespace QuanLyThuVienNhom05
 {
     public partial class DocGia : Form
     {
+        static ThuVienDBContext context = new ThuVienDBContext();
+        List<DOCGIA> listDocGia = context.DOCGIAs.ToList();
         public DocGia()
         {
             InitializeComponent();
